d39f8a6dee3058d6b3f290c3d59c570eee752be1
kalikalii <33070492+kalikalii@users.noreply.github.com>
2019-08-31 14:55:37 +0200

    responsive

--- scripts/main.js
+++ scripts/main.js
@@ -4,14 +4,23 @@ $(document).ready(function () {
     var $header = $('header');
     var $sticky = $header.before($header.clone().addClass("sticky"));
 
+    var x = window.matchMedia("(max-width: 700px)");
     $(window).on("scroll", function () {
         var scrollFromTop = $(window).scrollTop();
-        $("body").toggleClass("scroll", (scrollFromTop > 350));
-
-        if ($(window).scrollTop() > 1500) {
-            $(".loveText").addClass("scrolll");
+        if ($(body.hasClass("menu-active"))) {
+            $("body").toggleClass("scroll2", (scrollFromTop > 350));
+        } else {
+            $("body").toggleClass("scroll", (scrollFromTop > 350));
 
         }
+
+        // if ($(window).scrollTop() > 1500) {
+        //     if ($(body.hasClass("menu-active"))) {
+        //         $(".loveText").addClass("scrolll");
+        //     }
+
+
+        // }
 
     });
 
@@ -26,19 +35,34 @@ $(document).ready(function () {
         gutter: 0
 
     });
+    if (x) {
+        new Glider(document.querySelector('.glider'), {
+            slidesToShow: 1.5,
+            slidesToScroll: 1,
+            draggable: true,
+            rewind: true,
+            dots: '.dots',
+            arrows: {
+                prev: '.glider-prev',
+                next: '.glider-next'
+            }
+        });
 
-    new Glider(document.querySelector('.glider'), {
+    } else {
+        new Glider(document.querySelector('.glider'), {
+            slidesToShow: 2.5,
+            slidesToScroll: 1.5,
+            draggable: true,
+            rewind: true,
+            dots: '.dots',
+            arrows: {
+                prev: '.glider-prev',
+                next: '.glider-next'
+            }
+        });
+    }
 
-        slidesToShow: 2,
-        slidesToScroll: 1.5,
-        draggable: true,
-        rewind: true,
-        dots: '.dots',
-        arrows: {
-            prev: '.glider-prev',
-            next: '.glider-next'
-        }
-    });
+
 
     $('.menu li a[href^="#"]').on('click', function (fun) {
 
@@ -63,8 +87,12 @@ $(document).ready(function () {
         body.addClass('menu-active');
     });
 
-    $('.menu li a').on('click', function () {
+    mainOverlay.on('click', function () {
         body.removeClass('menu-active');
+    });
+
+    $('.menu li a').on('click', function () {
+        $('body').removeClass('menu-active');
     });
 
 });
